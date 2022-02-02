8af306eb73d5ac55034dc58a420f64cf7d0ca9b5
Ben Lenton <benlenton@gmail.com>
2022-02-02 09:12:29 +0000

    updated logic for handling edge cases

--- final_project.py
+++ final_project.py
@@ -1,8 +1,11 @@
-# Banking App Brief:
-# Class-based
-# Withdrawal and Deposit methods
-# Write all transactions to a python file
-# Repeatedly ask the user to withdraw or deposit
+"""
+Banking App Brief:
+Class-based
+Withdrawal and Deposit methods
+Write all transactions to a python file
+Repeatedly ask the user to withdraw or deposit
+"""
+
 
 class BankAccount:
     
@@ -18,14 +21,18 @@ class BankAccount:
     def withdraw(self):
         minus_amount = input("Please enter the amount you wish to withdraw: ")
         try:
-            self.transaction_amount = float(minus_amount) * -1
+            self.transaction_amount = float(minus_amount) * -1              
         except ValueError:
             self.transaction_amount = 0
         except Exception as e:
             print(f"Exception ({type(e)}): Transaction declined")
-        self.new_balance = self.current_balance + self.transaction_amount
-        self.current_balance = self.new_balance
-        return self.current_balance
+        if self.current_balance + self.transaction_amount > 0 and self.transaction_amount < 0:
+            self.new_balance = self.current_balance + self.transaction_amount
+            self.current_balance = self.new_balance
+            return self.current_balance
+        else:
+            self.new_balance = self.current_balance
+            print("Transaction Declined.")
 
     def deposit(self):
         positive_amount = input("Please enter the amount you wish to deposit: ")
@@ -35,9 +42,13 @@ class BankAccount:
             self.transaction_amount = 0
         except Exception as e:
             print(f"Exception ({type(e)}): Transaction declined")
-        self.new_balance = self.current_balance + self.transaction_amount
-        self.current_balance = self.new_balance
-        return self.current_balance
+        if self.current_balance + self.transaction_amount > 0 and self.transaction_amount > 0:
+            self.new_balance = self.current_balance + self.transaction_amount
+            self.current_balance = self.new_balance
+            return self.current_balance
+        else:
+            self.new_balance = self.current_balance
+            print("Transaction Declined.")
 
     def updateBalance(self):
         self.transaction_amount = str(self.transaction_amount)
@@ -51,27 +62,34 @@ banking_app = BankAccount()
 banking_app.displayBalance()
 
 while True:
-    choice = input("Select 1, 2, or 3 then press Enter: 1. Withdraw, 2. Deposit, 3. Check balance\t" )
+    choice = input("Select 1, 2, or 3 then press Enter: 1. Withdraw, 2. Deposit, 3. Check balance\t ")
 
     try:
         choice = int(choice)
     except Exception:
-        choice = input("Please re-enter a single number (1, 2, or 3)")
+        choice = input("Please re-enter a single number (1, 2, or 3) ")
     try:
         choice = int(choice)
     except Exception:
-        print("Transaction failed")
+        print("Transaction failed.")
         quit()
 
     if choice == 1:
         banking_app.withdraw()
+        print("\n")
         banking_app.updateBalance()
+        print("\n")
         banking_app.displayBalance()
+        print("\n")
     elif choice == 2:
         banking_app.deposit()
+        print("\n")
         banking_app.updateBalance()
+        print("\n")
         banking_app.displayBalance()
+        print("\n")
     elif choice == 3:
         banking_app.displayBalance()
+        print("\n")
     else:
-        print("transaction failed")
+        print("Transaction failed.\n")
